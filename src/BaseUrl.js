ef7e03727420be11cf399aac002a39b8265dff50
Abdul-Wahab <usamsaleem0@gmail.com>
2025-04-22 18:56:26 +0500

    update

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -8,4 +8,5 @@ export const BaseUrl ="https://test.api.amadeus.com"
 export const BackendUrl ="https://fly-arzan-backend-production.up.railway.app"
 // export const BackendUrl ="http://localhost:5000"
 export const CURR_API_KEY = "e6d032369e2646afa47b7a8a7d64f408";
-export const LOCATION_API_KEY = "362f957828c2e1d9699784f2788f2bd5";
+export const LOCATION_API_KEY = '362f957828c2e1d9699784f2788f2bd5';
+
