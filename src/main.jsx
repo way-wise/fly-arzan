697853ad0e7f9f611e50411b08de69711ab97d01
tauhid97k <tauhid.coder@gmail.com>
2025-08-15 23:14:34 +0600

    added slider cards to UserFul Articles section

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import 'antd/dist/reset.css'; // ya 'antd/dist/antd.css'
+import "antd/dist/reset.css"; // ya 'antd/dist/antd.css'
 
 import "./index.css";
 import "./App.css";
@@ -8,6 +8,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "react-toastify/dist/ReactToastify.css";
+import "@splidejs/react-splide/css";
 
 import { Provider } from "react-redux";
 import { store } from "./Redux/store.js";
@@ -15,24 +16,23 @@ import { CarProvider } from "./context/CarContext.jsx";
 import { LocationProvider } from "./context/userLocationContext.jsx";
 import { HotelProvider } from "./context/HotelContext.jsx";
 import { FlightProvider } from "./context/FlightContext.jsx";
-import './i18n'; // import here!
+import "./i18n"; // import here!
 import { CurrencyProvider } from "./context/CurrencyContext.jsx";
-
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <LocationProvider>
-  <CurrencyProvider>
-    <CarProvider>
-      <HotelProvider>
-        <FlightProvider>
-          <Provider store={store}>
-            <App />
-          </Provider>
-        </FlightProvider>
-      </HotelProvider>
-    </CarProvider>
-  </CurrencyProvider>
+    <CurrencyProvider>
+      <CarProvider>
+        <HotelProvider>
+          <FlightProvider>
+            <Provider store={store}>
+              <App />
+            </Provider>
+          </FlightProvider>
+        </HotelProvider>
+      </CarProvider>
+    </CurrencyProvider>
   </LocationProvider>
   // {/* // </StrictMode/> */}
 );
