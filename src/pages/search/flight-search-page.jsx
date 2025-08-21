4413b830f6c1b8f765882811cb9489dc779e3671
tauhid97k <tauhid.coder@gmail.com>
2025-08-21 13:32:43 +0600

    added flexible dates design for flight search page

--- src/pages/search/flight-search-page.jsx
+++ src/pages/search/flight-search-page.jsx
@@ -1,32 +1,70 @@
 import OneWayForm from "@/components/ui/hero-search-filter/flights/one-way-form";
-import { RadioGroup, RadioGroupItem } from "@radix-ui/react-radio-group";
 import { CalendarDays } from "lucide-react";
 
 const FlightSearchPage = () => {
-  return (
-    <div className="tw:py-6 tw:bg-[#F2FAFF]">
-      <div className="tw:container">
-        <h1 className="tw:!text-[18px] tw:font-semibold tw:text-[#00000B] tw:!mb-5">
-          Istanbul (IST) - Dubai (DXB) - 2 Travellers, Economy
-        </h1>
-        <div className="tw:rounded-xl tw:bg-white tw:shadow tw:!p-5">
-          <OneWayForm />
-        </div>
+  const flexibleDates = [
+    {
+      date: "31 Aug",
+      price: "$122",
+    },
+    {
+      date: "1 Sep",
+      price: "$173",
+    },
+    {
+      date: "2 Sep",
+      price: "$126",
+    },
+    {
+      date: "3 Sep",
+      price: "$129",
+    },
+    {
+      date: "4 Sep",
+      price: "$122",
+    },
+    {
+      date: "5 Sep",
+      price: "$141",
+    },
+    {
+      date: "6 Sep",
+      price: "$141",
+    },
+  ];
 
-        {/* Flexible Dates */}
-        <div className="tw:my-10 tw:flex tw:items-center tw:gap-[10px]">
-          <RadioGroup className="tw:rounded-xl tw:flex tw:items-center tw:grow tw:bg-white tw:shadow tw:divide-x tw:divide-muted">
-            <div className="tw:flex tw:flex-col tw:gap-1 tw:!py-[24px] tw:!px-[20px] tw:h-[93px] has-data-[state=checked]:tw:bg-primary has-data-[state=checked]:tw:text-white has-data-[state=checked]:tw:border-primary has-data-[state=checked]:tw:border-[1px] has-data-[state=checked]:tw:shadow-none has-data-[state=checked]:tw:ring-0 has-data-[state=checked]:tw:ring-offset-0">
-              <RadioGroupItem value="1" />
-              <span className="tw:text-[14px] tw:font-medium tw:text-secondary">
-                31 Aug
-              </span>
-              <span className="tw:text-[20px] tw:font-semibold tw:text-primary">
-                $122
-              </span>
-            </div>
-          </RadioGroup>
-          <div className="tw:rounded-xl tw:bg-white tw:shadow tw:flex tw:flex-col tw:items-center tw:gap-2 tw:!py-[24px] tw:!px-[20px] tw:h-[93px]">
+  return (
+    <div className="tw:flex tw:flex-col tw:min-h-screen">
+      <div className="tw:py-6 tw:bg-[#F2FAFF]">
+        <div className="tw:container">
+          <h1 className="tw:!text-[18px] tw:font-semibold tw:text-[#00000B] tw:!mb-5">
+            Istanbul (IST) - Dubai (DXB) - 2 Travellers, Economy
+          </h1>
+          <div className="tw:rounded-xl tw:bg-white tw:shadow tw:!p-5">
+            <OneWayForm />
+          </div>
+        </div>
+      </div>
+
+      {/* Flexible Dates */}
+      <div className="tw:py-10 tw:bg-[#EFF3F8] tw:grow">
+        <div className="tw:container tw:flex tw:items-center tw:gap-[10px]">
+          <div className="tw:rounded-xl tw:flex tw:items-center tw:shadow tw:grow tw:divide-x tw:divide-muted tw:bg-white">
+            {flexibleDates.map((date, index) => (
+              <label
+                key={index}
+                className="tw:!flex tw:flex-col tw:justify-center tw:gap-1 tw:!py-[24px] tw:!px-[20px] tw:!mb-0 tw:cursor-pointer tw:grow tw:text-center tw:h-[93px]"
+              >
+                <span className="tw:text-[14px] tw:font-medium tw:text-secondary">
+                  {date.date}
+                </span>
+                <span className="tw:text-[20px] tw:font-semibold tw:text-primary">
+                  {date.price}
+                </span>
+              </label>
+            ))}
+          </div>
+          <div className="tw:rounded-xl tw:bg-white tw:shadow tw:flex tw:flex-col tw:items-center tw:gap-2 tw:!py-[24px] tw:!px-[20px] tw:h-[93px] tw:shrink-0">
             <CalendarDays size={20} className="tw:text-secondary tw:shrink-0" />
             <p className="tw:text-[14px] tw:font-medium">Flexible Dates</p>
           </div>
