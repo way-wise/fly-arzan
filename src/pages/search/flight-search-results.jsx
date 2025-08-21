f14b2f2076665cd76729caa66440feb536b2e4b5
tauhid97k <tauhid.coder@gmail.com>
2025-08-21 22:07:40 +0600

    dynamic listing data from mock data added

--- src/pages/search/flight-search-results.jsx
+++ src/pages/search/flight-search-results.jsx
@@ -36,6 +36,346 @@ const FlightSearchResults = () => {
     },
   ];
 
+  const flightResults = [
+    {
+      id: 1,
+      price: 410,
+      totalPrice: 482,
+      logo: "/images/airlines/flyDubai.png",
+      flights: [
+        {
+          airline: "flydubai",
+          airlineCode: "FDB",
+          flightNumber: "1980",
+          departure: {
+            time: "18:35",
+            airport: "IST",
+          },
+          arrival: {
+            time: "00:05",
+            airport: "DBX",
+          },
+          duration: "4h 30m",
+          stops: "Direct",
+        },
+        {
+          airline: "flydubai",
+          airlineCode: "FDB",
+          flightNumber: "1980",
+          departure: {
+            time: "21:15",
+            airport: "DXB",
+          },
+          arrival: {
+            time: "01:10",
+            airport: "IST",
+          },
+          duration: "4h 55m",
+          stops: "Direct",
+        },
+      ],
+    },
+    {
+      id: 2,
+      price: 241,
+      totalPrice: 482,
+      logo: "/images/airlines/flyDubai.png",
+      flights: [
+        {
+          airline: "flydubai",
+          airlineCode: "FDB",
+          flightNumber: "1984",
+          departure: {
+            time: "02:10",
+            airport: "IST",
+          },
+          arrival: {
+            time: "07:40",
+            airport: "DBX",
+          },
+          duration: "4h 30m",
+          stops: "Direct",
+        },
+      ],
+    },
+    {
+      id: 3,
+      price: 129,
+      totalPrice: 482,
+      logo: "/images/airlines/azal.png",
+      flights: [
+        {
+          airline: "AZAL",
+          airlineCode: "AHY",
+          flightNumber: "771",
+          departure: {
+            time: "22:50",
+            airport: "IST",
+          },
+          arrival: {
+            time: "11:55",
+            airport: "DBX",
+          },
+          duration: "12h 05m",
+          stops: "1 Stop",
+        },
+      ],
+    },
+    {
+      id: 4,
+      price: 130,
+      totalPrice: 244,
+      logo: "/images/airlines/azal.png",
+      flights: [
+        {
+          airline: "AZAL",
+          airlineCode: "AHY",
+          flightNumber: "776",
+          departure: {
+            time: "14:35",
+            airport: "IST",
+          },
+          arrival: {
+            time: "11:55",
+            airport: "DBX",
+          },
+          duration: "20h 20m",
+          stops: "1 Stop",
+        },
+      ],
+    },
+    {
+      id: 5,
+      price: 130,
+      totalPrice: 244,
+      logo: "/images/airlines/azal.png",
+      flights: [
+        {
+          airline: "AZAL",
+          airlineCode: "AHY",
+          flightNumber: "776",
+          departure: {
+            time: "12:00",
+            airport: "IST",
+          },
+          arrival: {
+            time: "11:55",
+            airport: "DBX",
+          },
+          duration: "22h 55m",
+          stops: "1 Stop, GYD",
+        },
+      ],
+    },
+    {
+      id: 6,
+      price: 622,
+      totalPrice: 1244,
+      logo: "/images/airlines/emirates.png",
+      flights: [
+        {
+          airline: "Emirates",
+          airlineCode: "UAE",
+          flightNumber: "2300",
+          departure: {
+            time: "16:25",
+            airport: "IST",
+          },
+          arrival: {
+            time: "21:50",
+            airport: "DBX",
+          },
+          duration: "4h 25m",
+          stops: "Direct",
+        },
+      ],
+    },
+    {
+      id: 7,
+      price: 622,
+      totalPrice: 1244,
+      logo: "/images/airlines/emirates.png",
+      flights: [
+        {
+          airline: "Emirates",
+          airlineCode: "UAE",
+          flightNumber: "2150",
+          departure: {
+            time: "18:35",
+            airport: "IST",
+          },
+          arrival: {
+            time: "00:05",
+            airport: "DBX",
+          },
+          duration: "4h 30m",
+          stops: "Direct",
+        },
+      ],
+    },
+    {
+      id: 8,
+      price: 622,
+      totalPrice: 1244,
+      logo: "/images/airlines/emirates.png",
+      flights: [
+        {
+          airline: "Emirates",
+          airlineCode: "UAE",
+          flightNumber: "764",
+          departure: {
+            time: "19:25",
+            airport: "IST",
+          },
+          arrival: {
+            time: "00:55",
+            airport: "DBX",
+          },
+          duration: "4h 30m",
+          stops: "Direct",
+        },
+      ],
+    },
+    {
+      id: 9,
+      price: 266,
+      totalPrice: 532,
+      logo: "/images/airlines/mea.png",
+      flights: [
+        {
+          airline: "MEA",
+          airlineCode: "MEA",
+          flightNumber: "1376",
+          departure: {
+            time: "11:20",
+            airport: "IST",
+          },
+          arrival: {
+            time: "20:40",
+            airport: "DBX",
+          },
+          duration: "8h 20m",
+          stops: "1 Stop, BEY",
+        },
+      ],
+    },
+    {
+      id: 10,
+      price: 610,
+      totalPrice: 1482,
+      logo: "/images/airlines/turkish.png",
+      flights: [
+        {
+          airline: "Turkish Airlines",
+          airlineCode: "TK",
+          flightNumber: "1976",
+          departure: {
+            time: "21:00",
+            airport: "IST",
+          },
+          arrival: {
+            time: "02:30",
+            airport: "DXB",
+          },
+          duration: "4h 30m",
+          stops: "Direct",
+        },
+      ],
+    },
+    {
+      id: 11,
+      price: 610,
+      totalPrice: 1482,
+      logo: "/images/airlines/turkish.png",
+      flights: [
+        {
+          airline: "Turkish Airlines",
+          airlineCode: "TK",
+          flightNumber: "1989",
+          departure: {
+            time: "18:50",
+            airport: "IST",
+          },
+          arrival: {
+            time: "00:20",
+            airport: "DBX",
+          },
+          duration: "4h 30m",
+          stops: "Direct",
+        },
+      ],
+    },
+    {
+      id: 12,
+      price: 410,
+      totalPrice: 819,
+      logo: "/images/airlines/emirates.png",
+      flights: [
+        {
+          airline: "Emirates",
+          airlineCode: "UAE",
+          flightNumber: "1980",
+          departure: {
+            time: "02:10",
+            airport: "IST",
+          },
+          arrival: {
+            time: "7:40",
+            airport: "DBX",
+          },
+          duration: "4h 30m",
+          stops: "Direct",
+        },
+      ],
+    },
+    {
+      id: 13,
+      price: 610,
+      totalPrice: 1220,
+      logo: "/images/airlines/qatar.png",
+      flights: [
+        {
+          airline: "Qatar Airways",
+          airlineCode: "QTR",
+          flightNumber: "310",
+          departure: {
+            time: "13:45",
+            airport: "IST",
+          },
+          arrival: {
+            time: "12:20",
+            airport: "DBX",
+          },
+          duration: "4h 35m",
+          stops: "Direct",
+        },
+      ],
+    },
+    {
+      id: 14,
+      price: 610,
+      totalPrice: 1220,
+      logo: "/images/airlines/qatar.png",
+      flights: [
+        {
+          airline: "Qatar Airways",
+          airlineCode: "QTR",
+          flightNumber: "210",
+          departure: {
+            time: "00:25",
+            airport: "IST",
+          },
+          arrival: {
+            time: "05:55",
+            airport: "DBX",
+          },
+          duration: "4h 30m",
+          stops: "Direct",
+        },
+      ],
+    },
+  ];
+
   return (
     <div className="tw:flex tw:flex-col tw:gap-6">
       {/* List Header */}
@@ -89,45 +429,88 @@ const FlightSearchResults = () => {
       </div>
 
       {/* Available Flight List */}
-      <div className="tw:rounded-xl tw:bg-white tw:shadow tw:p-4 tw:flex tw:items-center tw:justify-between">
-        {/* Airline Logo, Code */}
-        <div className="tw:flex tw:flex-col tw:justify-center tw:items-center tw:gap-0.5">
-          <img src="/images/airlines/flyDubai.png" className="tw:h-[50px]" />
-          <span className="tw:text-sm tw:text-secondary">dlydubai</span>
-          <span className="tw:text-sm tw:text-secondary">FDB - 1984</span>
-        </div>
+      <div className="tw:grid tw:grid-cols-1 tw:gap-[30px]">
+        {flightResults.map((itinerary) => (
+          <div
+            key={itinerary.id}
+            className="tw:rounded-xl tw:bg-white tw:shadow tw:p-4 tw:flex tw:items-center tw:justify-between"
+          >
+            {/* Flight Details Section */}
+            <div className="tw:flex-grow tw:flex tw:flex-col tw:gap-4">
+              {itinerary.flights.map((flight, index) => (
+                // This div contains one leg of the journey (departure, arrival, etc.)
+                <div
+                  key={index}
+                  className="tw:flex tw:items-center tw:justify-between"
+                >
+                  {/* Airline Logo, Code */}
+                  <div className="tw:flex tw:flex-col tw:justify-center tw:items-center tw:gap-0.5 tw:w-1/4">
+                    <img
+                      src={itinerary.logo}
+                      alt={flight.airline}
+                      className="tw:w-[120px] tw:object-contain"
+                    />
+                    <span className="tw:text-sm tw:text-secondary">
+                      {flight.airline}
+                    </span>
+                    <span className="tw:text-sm tw:text-secondary">{`${flight.airlineCode} - ${flight.flightNumber}`}</span>
+                  </div>
 
-        {/* Time, Stop, Airline */}
-        <div className="tw:flex tw:items-center tw:gap-6">
-          {/* Depart */}
-          <div className="tw:flex tw:flex-col tw:gap-1 tw:text-right">
-            <span className="tw:font-semibold tw:text-[20px]">02:10</span>
-            <span className="tw:text-sm tw:text-[#5D586C]">IST</span>
-          </div>
-          {/* Duration & Stop */}
-          <div className="tw:flex tw:items-center tw:gap-2">
-            <div className="tw:flex tw:flex-col tw:text-center tw:gap-1">
-              <span className="tw:text-sm tw:font-semibold">4h 55</span>
-              <span className="tw:h-px tw:w-[82px] tw:bg-secondary" />
-              <span className="tw:text-sm tw:text-primary">Direct</span>
+                  {/* Time, Stop, Airline */}
+                  <div className="tw:flex tw:items-center tw:gap-6 tw:w-1/2">
+                    {/* Depart */}
+                    <div className="tw:flex tw:flex-col tw:gap-1 tw:text-right">
+                      <span className="tw:font-semibold tw:text-[20px]">
+                        {flight.departure.time}
+                      </span>
+                      <span className="tw:text-sm tw:text-[#5D586C]">
+                        {flight.departure.airport}
+                      </span>
+                    </div>
+                    {/* Duration & Stop */}
+                    <div className="tw:flex tw:items-center tw:gap-2">
+                      <div className="tw:flex tw:flex-col tw:text-center tw:gap-1">
+                        <span className="tw:text-sm tw:font-semibold">
+                          {flight.duration}
+                        </span>
+                        <span className="tw:h-px tw:w-[82px] tw:bg-secondary" />
+                        <span className="tw:text-sm tw:text-primary">
+                          {flight.stops}
+                        </span>
+                      </div>
+                      <RiPlaneLine
+                        size={24}
+                        className="tw:text-secondary tw:rotate-90"
+                      />
+                    </div>
+                    {/* Arrival */}
+                    <div className="tw:flex tw:flex-col tw:gap-1 tw:text-left">
+                      <span className="tw:font-semibold tw:text-[20px]">
+                        {flight.arrival.time}
+                      </span>
+                      <span className="tw:text-sm tw:text-[#5D586C]">
+                        {flight.arrival.airport}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
-            <RiPlaneLine size={24} className="tw:text-secondary tw:rotate-90" />
-          </div>
-          {/* Arrival */}
-          <div className="tw:flex tw:flex-col tw:gap-1 tw:text-left">
-            <span className="tw:font-semibold tw:text-[20px]">07:40</span>
-            <span className="tw:text-sm tw:text-[#5D586C]">DBX</span>
-          </div>
-        </div>
 
-        {/* Price Select Button */}
-        <div className="tw:py-4 tw:px-6 tw:bg-[#F2FAFF] tw:rounded-xl tw:flex tw:flex-col tw:items-center tw:gap-3">
-          <button className="tw:bg-primary tw:py-2 tw:px-[30px] tw:flex tw:flex-col tw:!text-white tw:!rounded-full">
-            <span className="tw:text-sm">Select</span>
-            <span className="tw:text-xl tw:font-medium">$241</span>
-          </button>
-          <span className="tw:text-sm tw:text-[#939393]">$482 Total</span>
-        </div>
+            {/* Price Select Button */}
+            <div className="tw:py-4 tw:px-6 tw:bg-[#F2FAFF] tw:rounded-xl tw:flex tw:flex-col tw:items-center tw:gap-3 tw:ml-4">
+              <button className="tw:bg-primary tw:py-2 tw:px-[30px] tw:flex tw:flex-col tw:!text-white tw:!rounded-full">
+                <span className="tw:text-sm">Select</span>
+                <span className="tw:text-xl tw:font-medium">
+                  ${itinerary.price}
+                </span>
+              </button>
+              <span className="tw:text-sm tw:text-[#939393]">
+                ${itinerary.totalPrice} Total
+              </span>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
