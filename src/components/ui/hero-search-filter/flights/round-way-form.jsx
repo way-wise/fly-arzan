5216d3b9dcc3075605588b3a0530235dc397c04a
tauhid97k <tauhid.coder@gmail.com>
2025-08-27 23:42:29 +0600

    added swap functionality to round way

--- src/components/ui/hero-search-filter/flights/round-way-form.jsx
+++ src/components/ui/hero-search-filter/flights/round-way-form.jsx
@@ -25,6 +25,7 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { string, number, object, date } from "yup";
 import { useNavigate } from "react-router-dom";
+import { cn } from "@/lib/utils";
 
 const city = [
   { id: 1, name: "New York", code: "NYC", state: "NY" },
@@ -43,6 +44,11 @@ const RoundWayForm = () => {
   const navigate = useNavigate();
   const [queryFrom, setQueryFrom] = useState("");
   const [queryTo, setQueryTo] = useState("");
+  const [isSwapped, setIsSwapped] = useState(false);
+
+  const handleSwap = () => {
+    setIsSwapped((prevValue) => !prevValue);
+  };
 
   const RoundWayFormSchema = object({
     flyingFrom: object()
@@ -139,89 +145,102 @@ const RoundWayForm = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
       <fieldset className="tw:grid tw:grid-cols-1 tw:md:grid-cols-2 tw:xl:grid-cols-4 tw:2xl:flex tw:items-center tw:gap-4">
-        {/* Flying From */}
-        <Combobox
-          value={flyingFrom}
-          virtual={{ options: filteredCityFrom }}
-          onChange={(value) => setValue("flyingFrom", value)}
-          onClose={() => setQueryFrom("")}
-        >
-          <div className="tw:relative tw:z-40 tw:grow">
-            <ComboboxInput
-              id="flyingFrom"
-              displayValue={(city) => city?.name || ""}
-              onChange={(event) => setQueryFrom(event.target.value)}
-              className="tw:peer tw:py-[10px] tw:px-5 tw:h-[62px] tw:block tw:w-full tw:border tw:!border-muted tw:text-[15px] tw:!font-semibold tw:rounded-lg tw:placeholder:text-transparent tw:focus:border-primary tw:focus-visible:tw:border-primary tw:focus-visible:outline-hidden tw:focus:ring-primary tw:disabled:opacity-50 tw:disabled:pointer-events-none tw:focus:pt-6 tw:focus:pb-2 tw:not-placeholder-shown:pt-6 tw:not-placeholder-shown:pb-2 tw:autofill:pt-6 tw:autofill:pb-2 tw:focus-visible:ring-0"
-              placeholder="From"
-            />
-            <label
-              htmlFor="flyingFrom"
-              className="tw:absolute tw:top-0 tw:start-0 tw:h-full tw:!p-[14px_20.5px] tw:text-[20px] tw:text-secondary tw:truncate tw:pointer-events-none tw:transition tw:ease-in-out tw:duration-100 tw:border tw:border-transparent tw:origin-[0_0] tw:peer-disabled:opacity-50 tw:peer-disabled:pointer-events-none tw:peer-focus:scale-80 tw:peer-focus:translate-x-0.5 tw:peer-focus:-translate-y-1.5 tw:peer-focus:text-secondary tw:peer-not-placeholder-shown:scale-80 tw:peer-not-placeholder-shown:translate-x-0.5 tw:peer-not-placeholder-shown:-translate-y-1.5 tw:peer-not-placeholder-shown:text-secondary"
-            >
-              From
-            </label>
-            <ComboboxOptions className="tw:w-[var(--input-width)] tw:2xl:w-72">
-              {({ option: city }) => (
-                <ComboboxOption value={city}>
-                  <GiCommercialAirplane className="tw:text-secondary" />
-                  <span>{city.name}</span>
-                </ComboboxOption>
+        <div className="tw:flex tw:gap-4 tw:relative">
+          {/* Flying From */}
+          <Combobox
+            value={flyingFrom}
+            virtual={{ options: filteredCityFrom }}
+            onChange={(value) => setValue("flyingFrom", value)}
+            onClose={() => setQueryFrom("")}
+          >
+            <div
+              className={cn(
+                "tw:relative tw:z-40 tw:grow",
+                isSwapped ? "tw:order-2" : "tw:order-1"
               )}
-            </ComboboxOptions>
-            <button
-              type="button"
-              className="tw:absolute tw:z-50 tw:top-[calc(100%-16px)] tw:md:top-[50%] tw:right-1/2 tw:translate-x-1/2 tw:md:translate-x-0 tw:md:-right-8 tw:bg-white tw:md:-translate-y-1/2 tw:h-[50px] tw:w-[50px] tw:inline-flex tw:items-center tw:justify-center tw:border tw:!border-muted tw:!rounded-full"
             >
-              <svg
-                width="20"
-                height="18"
-                viewBox="0 0 20 18"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+              <ComboboxInput
+                id="flyingFrom"
+                displayValue={(city) => city?.name || ""}
+                onChange={(event) => setQueryFrom(event.target.value)}
+                className="tw:peer tw:py-[10px] tw:px-5 tw:h-[62px] tw:block tw:w-full tw:border tw:!border-muted tw:text-[15px] tw:!font-semibold tw:rounded-lg tw:placeholder:text-transparent tw:focus:border-primary tw:focus-visible:tw:border-primary tw:focus-visible:outline-hidden tw:focus:ring-primary tw:disabled:opacity-50 tw:disabled:pointer-events-none tw:focus:pt-6 tw:focus:pb-2 tw:not-placeholder-shown:pt-6 tw:not-placeholder-shown:pb-2 tw:autofill:pt-6 tw:autofill:pb-2 tw:focus-visible:ring-0"
+                placeholder="From"
+              />
+              <label
+                htmlFor="flyingFrom"
+                className="tw:absolute tw:top-0 tw:start-0 tw:h-full tw:!p-[14px_20.5px] tw:text-[20px] tw:text-secondary tw:truncate tw:pointer-events-none tw:transition tw:ease-in-out tw:duration-100 tw:border tw:border-transparent tw:origin-[0_0] tw:peer-disabled:opacity-50 tw:peer-disabled:pointer-events-none tw:peer-focus:scale-80 tw:peer-focus:translate-x-0.5 tw:peer-focus:-translate-y-1.5 tw:peer-focus:text-secondary tw:peer-not-placeholder-shown:scale-80 tw:peer-not-placeholder-shown:translate-x-0.5 tw:peer-not-placeholder-shown:-translate-y-1.5 tw:peer-not-placeholder-shown:text-secondary"
               >
-                <path
-                  d="M3.46039 7.34263C3.69297 7.57559 4.07227 7.57559 4.30485 7.34263L5.07727 6.57021C5.31023 6.33763 5.31023 5.95833 5.07727 5.72574L4.30485 4.95333H14.9332C15.9214 4.95333 16.7251 5.7571 16.7251 6.74531C16.7251 7.23922 17.1272 7.64129 17.6211 7.64129H18.2184C18.7124 7.64129 19.1144 7.23922 19.1144 6.74531C19.1144 4.43963 17.2388 2.56402 14.9332 2.56402H4.30485L5.07727 1.79161C5.31023 1.55902 5.31023 1.17972 5.07727 0.947135L4.30485 0.174718C4.07227 -0.0582393 3.69297 -0.0582393 3.46039 0.174718L0.0873589 3.54737C-0.0291196 3.66422 -0.0291196 3.85313 0.0873589 3.96998L3.46039 7.34263Z"
-                  fill="#353978"
-                />
-                <path
-                  d="M15.6541 10.3299C15.4215 10.097 15.0425 10.097 14.8096 10.3299L14.0372 11.1023C13.8038 11.3357 13.8034 11.713 14.0372 11.9468L14.8096 12.7192H4.18128C3.19308 12.7192 2.3893 11.9154 2.3893 10.9272C2.3893 10.4333 1.98723 10.0312 1.49332 10.0312H0.895989C0.402075 10.0312 0 10.4333 0 10.9272C0 13.2329 1.8756 15.1085 4.18128 15.1085H14.8096L14.0372 15.8809C13.8038 16.1143 13.8034 16.4916 14.0372 16.7254L14.8096 17.4978C15.0426 17.7308 15.4211 17.7308 15.6541 17.4978L19.0271 14.1252C19.1436 14.0083 19.1436 13.8194 19.0271 13.7026L15.6541 10.3299Z"
-                  fill="#353978"
-                />
-              </svg>
-            </button>
-          </div>
-        </Combobox>
-        {/* Flying To */}
-        <Combobox
-          value={flyingTo}
-          virtual={{ options: filteredCityTo }}
-          onChange={(value) => setValue("flyingTo", value)}
-          onClose={() => setQueryTo("")}
-        >
-          <div className="tw:relative">
-            <ComboboxInput
-              id="flyingTo"
-              displayValue={(city) => city?.name || ""}
-              onChange={(event) => setQueryTo(event.target.value)}
-              className="tw:peer tw:py-[10px] tw:px-5 tw:h-[62px] tw:block tw:w-full tw:border tw:!border-muted tw:text-[15px] tw:!font-semibold tw:rounded-lg tw:placeholder:text-transparent tw:focus:border-primary tw:focus-visible:tw:border-primary tw:focus-visible:outline-hidden tw:focus:ring-primary tw:disabled:opacity-50 tw:disabled:pointer-events-none tw:focus:pt-6 tw:focus:pb-2 tw:not-placeholder-shown:pt-6 tw:not-placeholder-shown:pb-2 tw:autofill:pt-6 tw:autofill:pb-2 tw:focus-visible:ring-0"
-              placeholder="To"
-            />
-            <label
-              htmlFor="flyingTo"
-              className="tw:absolute tw:top-0 tw:start-0 tw:h-full tw:!p-[14px_20.5px] tw:text-[20px] tw:text-secondary tw:truncate tw:pointer-events-none tw:transition tw:ease-in-out tw:duration-100 tw:border tw:border-transparent tw:origin-[0_0] tw:peer-disabled:opacity-50 tw:peer-disabled:pointer-events-none tw:peer-focus:scale-80 tw:peer-focus:translate-x-0.5 tw:peer-focus:-translate-y-1.5 tw:peer-focus:text-secondary tw:peer-not-placeholder-shown:scale-80 tw:peer-not-placeholder-shown:translate-x-0.5 tw:peer-not-placeholder-shown:-translate-y-1.5 tw:peer-not-placeholder-shown:text-secondary"
+                From
+              </label>
+              <ComboboxOptions className="tw:w-[var(--input-width)] tw:2xl:w-72">
+                {({ option: city }) => (
+                  <ComboboxOption value={city}>
+                    <GiCommercialAirplane className="tw:text-secondary" />
+                    <span>{city.name}</span>
+                  </ComboboxOption>
+                )}
+              </ComboboxOptions>
+            </div>
+          </Combobox>
+          <button
+            type="button"
+            onClick={handleSwap}
+            className="tw:absolute tw:z-50 tw:top-[6px] tw:md:top-[50%] tw:left-1/2 tw:-translate-x-1/2 tw:bg-white tw:md:-translate-y-1/2 tw:h-[50px] tw:w-[50px] tw:inline-flex tw:items-center tw:justify-center tw:border tw:!border-muted tw:!rounded-full"
+          >
+            <svg
+              width="20"
+              height="18"
+              viewBox="0 0 20 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              To
-            </label>
-            <ComboboxOptions className="tw:w-[var(--input-width)] tw:2xl:w-72">
-              {({ option: city }) => (
-                <ComboboxOption value={city}>
-                  <GiCommercialAirplane className="tw:text-secondary" />
-                  <span>{city.name}</span>
-                </ComboboxOption>
+              <path
+                d="M3.46039 7.34263C3.69297 7.57559 4.07227 7.57559 4.30485 7.34263L5.07727 6.57021C5.31023 6.33763 5.31023 5.95833 5.07727 5.72574L4.30485 4.95333H14.9332C15.9214 4.95333 16.7251 5.7571 16.7251 6.74531C16.7251 7.23922 17.1272 7.64129 17.6211 7.64129H18.2184C18.7124 7.64129 19.1144 7.23922 19.1144 6.74531C19.1144 4.43963 17.2388 2.56402 14.9332 2.56402H4.30485L5.07727 1.79161C5.31023 1.55902 5.31023 1.17972 5.07727 0.947135L4.30485 0.174718C4.07227 -0.0582393 3.69297 -0.0582393 3.46039 0.174718L0.0873589 3.54737C-0.0291196 3.66422 -0.0291196 3.85313 0.0873589 3.96998L3.46039 7.34263Z"
+                fill="#353978"
+              />
+              <path
+                d="M15.6541 10.3299C15.4215 10.097 15.0425 10.097 14.8096 10.3299L14.0372 11.1023C13.8038 11.3357 13.8034 11.713 14.0372 11.9468L14.8096 12.7192H4.18128C3.19308 12.7192 2.3893 11.9154 2.3893 10.9272C2.3893 10.4333 1.98723 10.0312 1.49332 10.0312H0.895989C0.402075 10.0312 0 10.4333 0 10.9272C0 13.2329 1.8756 15.1085 4.18128 15.1085H14.8096L14.0372 15.8809C13.8038 16.1143 13.8034 16.4916 14.0372 16.7254L14.8096 17.4978C15.0426 17.7308 15.4211 17.7308 15.6541 17.4978L19.0271 14.1252C19.1436 14.0083 19.1436 13.8194 19.0271 13.7026L15.6541 10.3299Z"
+                fill="#353978"
+              />
+            </svg>
+          </button>
+          {/* Flying To */}
+          <Combobox
+            value={flyingTo}
+            virtual={{ options: filteredCityTo }}
+            onChange={(value) => setValue("flyingTo", value)}
+            onClose={() => setQueryTo("")}
+          >
+            <div
+              className={cn(
+                "tw:relative tw:grow",
+                isSwapped ? "tw:order-1" : "tw:order-2"
               )}
-            </ComboboxOptions>
-          </div>
-        </Combobox>
+            >
+              <ComboboxInput
+                id="flyingTo"
+                displayValue={(city) => city?.name || ""}
+                onChange={(event) => setQueryTo(event.target.value)}
+                className="tw:peer tw:py-[10px] tw:px-5 tw:h-[62px] tw:block tw:w-full tw:border tw:!border-muted tw:text-[15px] tw:!font-semibold tw:rounded-lg tw:placeholder:text-transparent tw:focus:border-primary tw:focus-visible:tw:border-primary tw:focus-visible:outline-hidden tw:focus:ring-primary tw:disabled:opacity-50 tw:disabled:pointer-events-none tw:focus:pt-6 tw:focus:pb-2 tw:not-placeholder-shown:pt-6 tw:not-placeholder-shown:pb-2 tw:autofill:pt-6 tw:autofill:pb-2 tw:focus-visible:ring-0"
+                placeholder="To"
+              />
+              <label
+                htmlFor="flyingTo"
+                className="tw:absolute tw:top-0 tw:start-0 tw:h-full tw:!p-[14px_20.5px] tw:text-[20px] tw:text-secondary tw:truncate tw:pointer-events-none tw:transition tw:ease-in-out tw:duration-100 tw:border tw:border-transparent tw:origin-[0_0] tw:peer-disabled:opacity-50 tw:peer-disabled:pointer-events-none tw:peer-focus:scale-80 tw:peer-focus:translate-x-0.5 tw:peer-focus:-translate-y-1.5 tw:peer-focus:text-secondary tw:peer-not-placeholder-shown:scale-80 tw:peer-not-placeholder-shown:translate-x-0.5 tw:peer-not-placeholder-shown:-translate-y-1.5 tw:peer-not-placeholder-shown:text-secondary"
+              >
+                To
+              </label>
+              <ComboboxOptions className="tw:w-[var(--input-width)] tw:2xl:w-72">
+                {({ option: city }) => (
+                  <ComboboxOption value={city}>
+                    <GiCommercialAirplane className="tw:text-secondary" />
+                    <span>{city.name}</span>
+                  </ComboboxOption>
+                )}
+              </ComboboxOptions>
+            </div>
+          </Combobox>
+        </div>
         {/* Depart */}
         <Popover open={departDateOpen} onOpenChange={setDepartDateOpen}>
           <PopoverTrigger asChild>
